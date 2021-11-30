654d4a4e48ac54bd511f5b17898df9980b83bae3
Peter Marton <petermarton@Peters-MacBook-Pro.local>
2021-11-30 13:32:25 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+const boom = new Audio();
+const clap = new Audio();
+const hihat = new Audio();
+const kick = new Audio();
+const openhat = new Audio();
+const ride = new Audio();
+const snare = new Audio();
+const tink = new Audio();
+const tom = new Audio();
+
+boom.src = "boom.wav";
+clap.src = "clap.wav"
+hihat.src = "hihat.wav"
+kick.src = "kick.wav"
+openhat.src = "openhat.wav"
+ride.src = "ride.wav"
+snare.src = "snare.wav"
+tink.src = "tink.wav"
+tom.src = "tom.wav"
+
+a = document.querySelector("#boom");
+s = document.querySelector("#clap");
+d = document.querySelector("#hihat");
+f = document.querySelector("#kick");
+g = document.querySelector("#openhat");
+h = document.querySelector("#ride");
+j = document.querySelector("#snare");
+k = document.querySelector("#tink");
+l = document.querySelector("#tom");
+
+function tryThis(aSound) {
+    aSound.play();
+}
+
+document.addEventListener("keydown", (event) => {
+    if (event.key == "a") {
+        tryThis(boom);
+        a.style.backgroundColor = "tomato";
+        setTimeout(function(){ a.style.backgroundColor = "white"; }, 500);
+    } else if (event.key == "s") {
+        tryThis(clap);
+        s.style.backgroundColor = "tomato";
+        setTimeout(function(){ s.style.backgroundColor = "white"; }, 500);
+    } else if (event.key == "d") {
+        tryThis(hihat);
+        d.style.backgroundColor = "tomato";
+        setTimeout(function(){ d.style.backgroundColor = "white"; }, 500);
+    } else if (event.key == "f") {
+        tryThis(kick);
+        f.style.backgroundColor = "tomato";
+        setTimeout(function(){ f.style.backgroundColor = "white"; }, 500);
+    } else if (event.key == "g") {
+        tryThis(openhat);
+        g.style.backgroundColor = "tomato";
+        setTimeout(function(){ g.style.backgroundColor = "white"; }, 500);
+    } else if (event.key == "h") {
+        tryThis(ride);
+        h.style.backgroundColor = "tomato";
+        setTimeout(function(){ h.style.backgroundColor = "white"; }, 500);
+    } else if (event.key == "j") {
+        tryThis(snare);
+        j.style.backgroundColor = "tomato";
+        setTimeout(function(){ j.style.backgroundColor = "white"; }, 500);
+    } else if (event.key == "k") {
+        tryThis(tink);
+        k.style.backgroundColor = "tomato";
+        setTimeout(function(){ k.style.backgroundColor = "white"; }, 500);
+    } else if (event.key == "l") {
+        tryThis(tom);
+        l.style.backgroundColor = "tomato";
+        setTimeout(function(){ l.style.backgroundColor = "white"; }, 500);
+    } else {
+        alert("Wrong character, please try again!");
+        alert("Press: A, S, D, F, G, H, J, K, L.");
+    }
+})
