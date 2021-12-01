8bfa8ecb09f95f591a84e6c73c96e495fd2dc344
Peter Marton <petermarton@Peters-MacBook-Pro.local>
2021-12-01 10:38:47 +0000

    initial commit

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ j = document.querySelector("#snare");
 k = document.querySelector("#tink");
 l = document.querySelector("#tom");
 
-function tryThis(aSound) {
-    aSound.play();
+function tryThis(drum) {
+    drum.play();
 }
 
 document.addEventListener("keydown", (event) => {
@@ -37,38 +37,47 @@ document.addEventListener("keydown", (event) => {
         tryThis(boom);
         a.style.backgroundColor = "tomato";
         setTimeout(function(){ a.style.backgroundColor = "white"; }, 500);
+        boom.currentTime = 0;
     } else if (event.key == "s") {
         tryThis(clap);
         s.style.backgroundColor = "tomato";
         setTimeout(function(){ s.style.backgroundColor = "white"; }, 500);
+        clap.currentTime = 0;
     } else if (event.key == "d") {
         tryThis(hihat);
         d.style.backgroundColor = "tomato";
         setTimeout(function(){ d.style.backgroundColor = "white"; }, 500);
+        hihat.currentTime = 0;
     } else if (event.key == "f") {
         tryThis(kick);
         f.style.backgroundColor = "tomato";
         setTimeout(function(){ f.style.backgroundColor = "white"; }, 500);
+        kick.currentTime = 0;
     } else if (event.key == "g") {
         tryThis(openhat);
         g.style.backgroundColor = "tomato";
         setTimeout(function(){ g.style.backgroundColor = "white"; }, 500);
+        openhat.currentTime = 0;
     } else if (event.key == "h") {
         tryThis(ride);
         h.style.backgroundColor = "tomato";
         setTimeout(function(){ h.style.backgroundColor = "white"; }, 500);
+        ride.currentTime = 0;
     } else if (event.key == "j") {
         tryThis(snare);
         j.style.backgroundColor = "tomato";
         setTimeout(function(){ j.style.backgroundColor = "white"; }, 500);
+        snare.currentTime = 0;
     } else if (event.key == "k") {
         tryThis(tink);
         k.style.backgroundColor = "tomato";
         setTimeout(function(){ k.style.backgroundColor = "white"; }, 500);
+        tink.currentTime = 0;
     } else if (event.key == "l") {
         tryThis(tom);
         l.style.backgroundColor = "tomato";
         setTimeout(function(){ l.style.backgroundColor = "white"; }, 500);
+        tom.currentTime = 0;
     } else {
         alert("Wrong character, please try again!");
         alert("Press: A, S, D, F, G, H, J, K, L.");
